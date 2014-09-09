d61e4bd3749e74783c2f3c53369909a51f446da7
Steven Miller <mmiller89@gatech.edu>
2014-09-08 20:33:04 -0400

    Add (safeish) password processing

--- bankinate.py
+++ bankinate.py
@@ -1,10 +1,19 @@
+import getpass
 import requests
 from bs4 import BeautifulSoup
 
+# Username/Password Input
 username = raw_input('Enter your username: ')
-print username
-payload = {'UserName': username, 'input': 'Go'} 
-r = requests.post("https://online.bbt.com/auth/pwd.tb", data = payload)
+password = getpass.getpass('Enter your password: ')
 
-print (r.text)
+# Parameters for Post
+request_payload = {'BrowserDetective': 'General Inquiry',
+                   'var_field': "",
+                   'UserName': username, 'inq': 'O', 'Password': password}
+
+# 
+r = requests.post("https://online.bbt.com/auth/pwd.tb", data=request_payload)
+
+f = open('stuff.html', 'w')
+f.write(r.text)
 
