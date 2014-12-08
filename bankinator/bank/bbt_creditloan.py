9bf0c2835b905ba1157af46c0b236f915941ae82
Steven Miller <mmiller89@gatech.edu>
2014-12-08 03:02:40 -0500

    Parse data to a list of lists
    This should be easier to work with than a Dictionary with column mapping

--- bankinator/bank/bbt_creditloan.py
+++ bankinator/bank/bbt_creditloan.py
@@ -61,18 +61,17 @@ class Bank(BankBase):
     def parse(self, account, account_text):
         account_soup = BeautifulSoup(account_text)
         account_headers = []
-        account_transactions = {}
+        account_transactions = []
 
-        # Retain the original order of the headers so the table rows eventually
-        # match the columns
         for header in account_soup.table.thead.tr.find_all('th'):
             account_headers.append(header.get_text())
-            account_transactions[header.get_text()] = []
+
+        account_transactions.append(account_headers)
 
         for row in account_soup.table.tbody.find_all('tr'):
-            account_transactions[account_headers[0]].append(row.th.get_text())
-
-            for index, table_data in enumerate(row.find_all('td')):
-                account_transactions[account_headers[index + 1]].append(''.join(table_data.get_text()).strip().split())
+            transaction_row = [row.th.get_text()]
+            for table_data in row.find_all('td'):
+                transaction_row.append(' '.join(table_data.get_text().strip().split()))
+            account_transactions.append(transaction_row)
 
         return account, account_transactions
